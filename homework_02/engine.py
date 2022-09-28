6bc3c3e04b4201560eed307ff4bee42c402c77bc
w84fate <pmaximandr@gmail.com>
2022-09-29 00:05:46 +0300

    fix engine

--- homework_02/engine.py
+++ homework_02/engine.py
@@ -7,9 +7,3 @@ from dataclasses import dataclass
 class Engine:
     volume: float
     pistons: int
-
-    def __post_init__(self):
-        if not isinstance(self.volume, float):
-            raise ValueError('value not an float')
-        if not isinstance(self.pistons, int):
-            raise ValueError('value not an int')
